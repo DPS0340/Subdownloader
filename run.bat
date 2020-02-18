50cae2dba0f396613239b106ba041a1e650cc8d0
jiho lee <jiholee@kpu.ac.kr>
2020-02-18 20:13:48 +0900

    .

--- run.bat
+++ run.bat
@@ -3,7 +3,7 @@ setlocal EnableDelayedExpansion
 set argCount=0
 
 SET mypath=%~dp0
-SET line=python3 %mypath:~0,-1%\src\wrapper.py
+SET line=py -3 %mypath:~0,-1%\src\wrapper.py
 
 for %%x in (%*) do (
     echo %%x
