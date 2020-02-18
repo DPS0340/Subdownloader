7dd93eb3167c267d27b57907cd24bcecaff3f123
jiho lee <jiholee@kpu.ac.kr>
2020-02-18 20:05:48 +0900

    .

--- src/wrapper.py
+++ src/wrapper.py
@@ -1,5 +1,5 @@
 import sys
-from os import system, listdir
+from os import listdir
 from os.path import dirname, basename, exists, isdir, abspath, normpath
 from settings import PYTHON_COMMAND
 import re
