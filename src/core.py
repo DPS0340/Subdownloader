bf4c56128f0ade63a8d3f68b4187bfd23a2f7c28
jiho lee <jiholee@kpu.ac.kr>
2020-02-18 18:48:31 +0900

    .

--- src/core.py
+++ src/core.py
@@ -11,7 +11,7 @@ def requestSoup(url, parser=PARSER):
     return BeautifulSoup(html.text, parser)
 
 def saveBinaryFile(blob, dest, name, ext):
-    with open('"%s/%s.%s"' % (dest, name, ext), 'wb') as w:
+    with open('"%s/%s.%s"' % (dest, name, ext), 'wb+') as w:
         w.write(blob)
     print('"%s/%s.%s"' % (dest, name, ext))
 
